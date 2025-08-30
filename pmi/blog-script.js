fd9811b39c19f01c470fcd2910052b16f41c461b
Anynomous <156112254+yosintv2@users.noreply.github.com>
2025-08-30 22:23:57 +0900

    Update blog-script.js

--- pmi/blog-script.js
+++ pmi/blog-script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const hours = Math.floor(diff / 3600000);
         const minutes = Math.floor((diff % 3600000) / 60000);
         const seconds = Math.floor((diff % 60000) / 1000);
-        return `Starts: ${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
       };
       const updateProgressBar = (start, duration, container, bar, text) => {
         const now = new Date();
