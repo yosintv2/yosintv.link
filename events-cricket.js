2d5a792e43a798e84e2c5365f189343e95754b96
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-05-21 00:15:35 +0900

    Update events-cricket.js

--- events-cricket.js
+++ events-cricket.js
@@ -1,25 +1,16 @@
 {
   "title": "All Cricket Events",
-  "matches": [     
-    
+  "matches": [
     {
       "name": " All Cricket Matches ",
       "image": "https://cdn.jsdelivr.net/gh/yosintv2/cdn/cricket.png",
       "link": "https://www.yosintv.net/cricket.html"
     },
-  
-    {
-      "name": " Nepal vs UAE - T20 Series 2026 ",
-      "image": "https://cricketvectors.akamaized.net/Series/2GW.png",
-      "link": "https://www.yosintv.net/cricket.html"
-    },
-    
     {
       "name": " Indian Premier League 2026 ",
       "image": "https://api.sofascore1.com/api/v1/unique-tournament/11165/image",
       "link": "https://www.yosintv.net/cricket.html"
     },
-    
     {
       "name": " Pakistan Super League 2026 ",
       "image": "https://api.sofascore1.com/api/v1/unique-tournament/11170/image",
